0a3ee4f8750a54137a538fbc970c13ec6fca5742
paulasotoglz <113448947+paulasotoglz@users.noreply.github.com>
2022-10-18 22:39:33 -0600

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,30 @@
+mySprite: Sprite = None
+
+def on_a_pressed():
+    if mySprite.x < scene.screen_width() / 2:
+        info.change_score_by(1)
+    if mySprite.y < scene.screen_height() / 2:
+        info.change_life_by(-1)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+mySprite = sprites.create(img("""
+        . . . . . . 5 . 5 . . . . . . .
+        . . . . . f 5 5 5 f f . . . . .
+        . . . . f 1 5 2 5 1 6 f . . . .
+        . . . f 1 6 6 6 6 6 1 6 f . . .
+        . . . f 6 6 f f f f 6 1 f . . .
+        . . . f 6 f f d d f f 6 f . . .
+        . . f 6 f d f d d f d f 6 f . .
+        . . f 6 f d 3 d d 3 d f 6 f . .
+        . . f 6 6 f d d d d f 6 6 f . .
+        . f 6 6 f 3 f f f f 3 f 6 6 f .
+        . . f f d 3 5 3 3 5 3 d f f . .
+        . . f d d f 3 5 5 3 f d d f . .
+        . . . f f 3 3 3 3 3 3 f f . . .
+        . . . f 3 3 5 3 3 5 3 3 f . . .
+        . . . f f f f f f f f f f . . .
+        . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(mySprite, 100, 100)
+info.set_life(5)
---
+++ main.ts
@@ -0,0 +1,29 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.x < scene.screenWidth() / 2) {
+        info.changeScoreBy(1)
+    }
+    if (mySprite.y < scene.screenHeight() / 2) {
+        info.changeLifeBy(-1)
+    }
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f f f c c f f f c . 
+    f f f c f f f f f f f c . 
+    c c c f f f e e f f c c . 
+    f f f f f e e f f c c f . 
+    f f f b f e e f b f f f . 
+    . f 4 1 f 4 4 f 1 4 f . . 
+    . f e 4 4 4 4 4 4 e f . . 
+    . f f f e e e e f f f . . 
+    f e f b 7 7 7 7 b f e f . 
+    e 4 f 7 7 7 7 7 7 f 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+info.setLife(5)
